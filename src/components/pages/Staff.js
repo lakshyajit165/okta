016501295f8b7bd0c9c806ee8868b948b92dc60d
lakshyajit165 <lakshyajit165@gmail.com>
2018-10-23 19:08:32 +0530

    Committing my project

--- src/components/pages/Staff.js
+++ src/components/pages/Staff.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function Staff() {
-  return (
-    <div>
-      <h1>Staff</h1>
-    </div>
-  )
+class Staff extends Component {
+
+  state = {
+      currentUserName: '',
+      currentUserEmail: ''
+  }  
+
+  componentDidMount(){
+      const idToken = JSON.parse(localStorage.getItem('okta-token-storage'));
+      this.setState({
+        currentUserEmail: idToken.idToken.claims.email,
+        currentUserName: idToken.idToken.claims.name,
+      });
+  }
+  render() {
+    
+    const { currentUserEmail, currentUserName} = this.state;
+    return (
+      <div className="jumbotron text-center">
+        <h1>Welcome {currentUserName}</h1>
+        <p className="lead"><b>Email: </b>{currentUserEmail}</p>
+        <hr/>
+        <p className="lead">You have reached the authorized area of the portal</p>
+      </div>
+    )
+  }
 }
+
+export default Staff;
